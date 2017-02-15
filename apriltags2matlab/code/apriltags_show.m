058619bf3ff58e0abe9743b72ba51c8b1f5b89e5
jiangfalcon <jiangfalcon@users.noreply.github.com>
2017-02-15 22:02:06 +0800

    modified version
    correct and rename the variables
    add new annotations

--- apriltags2matlab/code/apriltags_show.m
+++ apriltags2matlab/code/apriltags_show.m
@@ -6,25 +6,25 @@ fprintf('Initializing hardware...\n');
 % Connect to ROS master
 setenv('ROS_MASTER_URI','http://192.168.1.94:11311')
 setenv('ROS_IP','192.168.1.80')
-rosinit; % for testing
+rosinit;
 
 tag_sub = rossubscriber('/camera/tag_detections');
 camera_info_sub = rossubscriber('/left_cam/camera_info');
 color_point_sub = rossubscriber('/color_point');
-% tag_msg = rosmessage(tag_sub);
+
 fprintf('Done.\n');  
 %%
 % struct declare
-num = struct('id',[],'tag_dection',[]);
-color = struct('id','tag_dection');
+num = struct('id',[],'tag_detection',[]);
+color = struct('id','tag_detection');
 
 %%
 camera_info_data = receive(camera_info_sub, 10);
 fx = camera_info_data.K(1);
 fy = camera_info_data.K(5);    
-color = ['ro'; 'yo'; 'ko'; 'bo'];
-color_arrow = ['r'; 'y'; 'k'; 'b'];
-teg0_check = 0;
+color = ['ro'; 'yo'; 'ko'; 'bo']; %¡@set colors which represent each tags
+color_arrow = ['r'; 'y'; 'k'; 'b']; %¡@set colors which represent each tags
+tag0_check = 0; % existence checking variable of tag0 (0:inexist, 1:exist)
 
 while 1;    
     fprintf('----------------------------------------\n'); 
@@ -33,15 +33,15 @@ while 1;
     color_point_data = receive(color_point_sub, 10);
     
     dections = tag_data.Detections;
-    num.tag_dection = size(dections,1)
+    num.tag_detection = size(dections,1)
     
     % avoid the interruption caused by zero tag dection
-    if num.tag_dection == 0 
+    if num.tag_detection == 0 
         continue;
     end
     
     % extract massages
-    for i = 1:num.tag_dection
+    for i = 1:num.tag_detection
     id = dections(i).Id;
     num.id = id + 1;
     
@@ -49,7 +49,7 @@ while 1;
     pixcoord_tag{num.id}(1) = round(dections(i).Pixcoord.X);
     pixcoord_tag{num.id}(2) = camera_info_data.Height - round(dections(i).Pixcoord.Y); % set the pixel coordinate of tag0 and make the lower-left corner to be (0,0)
     
-    % extract pixel coordinate of apriltags
+    % extract pose of apriltags
     pos{num.id}(1) = dections(i).Pose.Pose.Position.X;
     pos{num.id}(2) = -dections(i).Pose.Pose.Position.Y;
     pos{num.id}(3) = dections(i).Pose.Pose.Position.Z;
@@ -59,7 +59,7 @@ while 1;
     quat{num.id}(3) = dections(i).Pose.Pose.Orientation.Z;
     quat{num.id}(4) = dections(i).Pose.Pose.Orientation.W;
     
-    % calculate Euler angles
+    % calculate Euler angles of tags
     eulZYX{num.id} = -quat2eul(quat{num.id});
     
     
@@ -77,7 +77,8 @@ while 1;
     hold on
     
     % plot color point
-    if color_point_data.Point.Z == 1
+    if color_point_data.Point.Z == 1 % Z is the detection state (0:inexist 1:exist)
+        % calculate the vector of color point w.r.t. tag0
         px = color_point_data.Point.X;
         py = double(camera_info_data.Height) - color_point_data.Point.Y;
     else
@@ -85,23 +86,28 @@ while 1;
         py = [];
     end
     
-    
-    if i == num.tag_dection 
-        for j = 1:num.tag_dection
-            teg0_check = teg0_check || dections(j).Id == 0;
+    % plot color point
+    if i == num.tag_detection 
+        % check if tag0 exist or not
+        for j = 1:num.tag_detection
+            tag0_check = tag0_check || (dections(j).Id == 0);
         end
-        if teg0_check == 1
+        
+        if tag0_check == 1 % tag0 exist
+            % mesaure anverage distance between tags and camera
             Pz_temp = 0;
-            for k = 1:num.tag_dection
+            for k = 1:num.tag_detection
                 id = dections(k).Id;
                 num.id = id + 1;
                 Pz_temp = Pz_temp + pos{num.id}(3);
             end
-            Pz = Pz_temp/num.tag_dection;
+            Pz = Pz_temp/num.tag_detection;
+            % calculate the position of color point
             Px = Pz*(px - pixcoord_tag{1}(1))/fx + pos{1}(1);
             Py = Pz*(py - pixcoord_tag{1}(2))/fy + pos{1}(2);
+            
             plot(Px,Py,'g.','MarkerSize',20);
-            teg0_check = 0; % reset teg0_check
+            tag0_check = 0; % reset tag0_check
         end
     end
     
